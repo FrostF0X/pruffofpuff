6b7482d438cbe5d3d85a08aff6b583659b7d4864
dims <dmytro.konowalow@gmail.com>
2024-09-08 09:27:50 +0200

    test

--- app/packages/nextjs/app/page.tsx
+++ app/packages/nextjs/app/page.tsx
@@ -5,15 +5,14 @@ import type {NextPage} from "next";
 import {useAccount} from "wagmi";
 import {BugAntIcon, MagnifyingGlassIcon} from "@heroicons/react/24/outline";
 import {Address} from "~~/components/scaffold-eth";
-import {useScaffoldReadContract} from "~~/hooks/scaffold-eth";
 
 const Home: NextPage = () => {
     const {address: connectedAddress} = useAccount();
-    const data = useScaffoldReadContract({
-        contractName: "PruffOfPuff",
-        functionName: "isPruffer",
-        args: [connectedAddress],
-    });
+    // const data = useScaffoldReadContract({
+    //     contractName: "PruffOfPuff",
+    //     functionName: "isPruffer",
+    //     args: [connectedAddress],
+    // });
     return (
         <>
             <div className="flex items-center flex-col flex-grow pt-10">
